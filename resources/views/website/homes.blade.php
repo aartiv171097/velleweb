10cc08b5f684c009cafb519cac38da41ed7cc771
Nitin Sharma <nitin.sharma649@gmail.com>
2019-03-16 05:45:49 +0000

    Last commit before change server

--- resources/views/website/homes.blade.php
+++ resources/views/website/homes.blade.php
@@ -5,12 +5,12 @@
             <div class="slider-item text-center" data-image="{{ asset('asset/images/slider-bg.jpg') }}" style="background-position:top">
                 <div class="slider-text text-left">
                     <div class="container">
-                        <div class="col-md-5 p-0 fade-in-left animate-effect">
-                            <p class="f-22 mb-0 mt-5 pt-5 text-white text-uppercase title"><strong>Show Your Ad Inside the<br>Instagram Story of Influencers</strong></p>
+                        <div class="col-md-5 p-0">
+                            <p class="f-22 mb-0 mt-5 pt-5 text-white text-uppercase title">Show Your Ad Inside the<br>Instagram Story of Influencers</p>
                             <img src="{{ asset('asset/images/p-line.png')}}" />
-                            <p class="text-white mt-4 f-15"><strong><strong>We find right  Influencers to show your<br>Content inside their personal story</strong></strong></p>
+                            <p class="text-white mt-4 f-15">We find right  Influencers to show your<br>Content inside their personal story</p>
                         </div>
-                        <div class="col-md-7 p-0 text-right fadeInRight animate-effect" data-wow-delay="0.3s">
+                        <div class="col-md-7 p-0 text-right">
                             <img src="{{ asset('asset/images/slider-img.png')}}" />
                         </div>
                     </div>
@@ -32,77 +32,28 @@
                     <img src="{{ asset('asset/images/section-line.png')}}" style="margin-left:-90px" />
                 </div>
                 
-                <div class="col-md-10 col-md-offset-1">
-                	<div class="row">
-                    	<div class="col-md-7 fade-in-left wow" data-wow-delay="0.2s">
-                            <div class="what-box" style="border-bottom-right-radius:0;">
-                                <div class="row">
-                                    <div class="col-md-2">
-                                        <img src="{{ asset('asset/images/dum.jpg')}}" class="img-border-radius" width="80" />
-                                    </div>
-                                    <div class="col-md-9 mt-3">
-                                        <p class="f-22"><strong>Love & Passion</strong></p>
-                                    </div>
-                                    <div class="col-md-12 mt-4">
-                                        Bring Genuine & Passionate influencer together.
-                                    </div>
-                                </div>
-                                                   
-                            </div>                
-                        </div>
-                        
-                        <div class="col-md-7 col-md-offset-5 fadeInRight wow" data-wow-delay="0.3s">
-                            <div class="what-box" style="border-bottom-right-radius:0;">
-                                <div class="row">
-                                    <div class="col-md-2">
-                                        <img src="{{ asset('asset/images/dum.jpg')}}" class="img-border-radius" width="80" />
-                                    </div>
-                                    <div class="col-md-9 mt-3">
-                                        <p class="f-22"><strong>Love & Passion</strong></p>
-                                    </div>
-                                    <div class="col-md-12 mt-4">
-                                        Bring Genuine & Passionate influencer together.
-                                    </div>
-                                </div>
-                                                   
-                            </div>                
-                        </div>
-                        
-                        <div class="col-md-7 fade-in-left wow" data-wow-delay="0.6s">
-                            <div class="what-box" style="border-bottom-right-radius:0;">
-                                <div class="row">
-                                    <div class="col-md-2">
-                                        <img src="{{ asset('asset/images/dum.jpg')}}" class="img-border-radius" width="80" />
-                                    </div>
-                                    <div class="col-md-9 mt-3">
-                                        <p class="f-22"><strong>Love & Passion</strong></p>
-                                    </div>
-                                    <div class="col-md-12 mt-4">
-                                        Bring Genuine & Passionate influencer together.
-                                    </div>
-                                </div>
-                                                   
-                            </div>                
-                        </div>
-                        
-                        <div class="col-md-7 col-md-offset-5 fadeInRight wow" data-wow-delay="0.7s">
-                            <div class="what-box" style="border-bottom-right-radius:0;">
-                                <div class="row">
-                                    <div class="col-md-2">
-                                        <img src="{{ asset('asset/images/dum.jpg')}}" class="img-border-radius" width="80" />
-                                    </div>
-                                    <div class="col-md-9 mt-3">
-                                        <p class="f-22"><strong>Love & Passion</strong></p>
-                                    </div>
-                                    <div class="col-md-12 mt-4">
-                                        Bring Genuine & Passionate influencer together.
-                                    </div>
-                                </div>
-                                                   
-                            </div>                
-                        </div>
+                <div class="col-md-6">
+                    <div class="what-box" style="border-bottom-right-radius:0">
+                        nulla ac est pellentesque, ut convallis purus semper.                    
+                    </div>                
+                </div>
                 
-                	</div>
+                <div class="col-md-6">
+                    <div class="what-box" style="border-bottom-left-radius:0">
+                        nulla ac est pellentesque, ut convallis purus semper.                    
+                    </div>                
+                </div>
+                
+                <div class="col-md-6">
+                    <div class="what-box" style="border-top-right-radius:0">
+                        nulla ac est pellentesque, ut convallis purus semper.                    
+                    </div>                
+                </div>
+                
+                <div class="col-md-6">
+                    <div class="what-box" style="border-top-left-radius:0">
+                        nulla ac est pellentesque, ut convallis purus semper.                    
+                    </div>                
                 </div>
             
             </div>
@@ -120,52 +71,19 @@
                 </div> 
             </div>                 
          </div> 
-          
-         	<div class="row m-0 position-rel">
-                <img src="{{ asset('asset/images/story-bg.jpg')}}" class="fadeInRight wow" />
-                <div class="container">
-                    <div class="story-text fade-in-left wow">
-                        <p class="section-col f-18 mb-5" style="line-height:25px"><img src="{{ asset('asset/images/dum.jpg')}}" class="float-left" width="80" /> <span class="float-left ml-4">Fresh Hot Pizza in 30 min or Less -<br />Domino's Pizza</span></p>
-                        <div class="clearfix"></div>
-                        <p class="f-16 mt-5"><img src="{{ asset('asset/images/imp.jpg')}}" /> Impressions</p>
-                        <p class="f-16"><img src="{{ asset('asset/images/reach.jpg')}}" /> Reach</p>
-                        <p class="f-16"><img src="{{ asset('asset/images/number.jpg')}}" /> Number of Influencers</p>
-                        
-                        
-                    </div>
-                </div>                
-            </div>
-             
-          <div class="row m-0 position-rel">
-                <img src="{{ asset('asset/images/story-bg.jpg')}}" class="fadeInRight wow" />
-                <div class="container">
-                    <div class="story-text fade-in-left wow">
-                        <p class="section-col f-18 mb-5" style="line-height:25px"><img src="{{ asset('asset/images/dum.jpg')}}" class="float-left" width="80" /> <span class="float-left ml-4">Fresh Hot Pizza in 30 min or Less -<br />Domino's Pizza</span></p>
-                        <div class="clearfix"></div>
-                        <p class="f-16 mt-5"><img src="{{ asset('asset/images/imp.jpg')}}" /> Impressions</p>
-                        <p class="f-16"><img src="{{ asset('asset/images/reach.jpg')}}" /> Reach</p>
-                        <p class="f-16"><img src="{{ asset('asset/images/number.jpg')}}" /> Number of Influencers</p>
-                        
-                        
-                    </div>
-                </div>                
-            </div>
-            
-            <div class="row m-0 position-rel">
-                <img src="{{ asset('asset/images/story-bg.jpg')}}" class="fadeInRight wow" />
-                <div class="container">
-                    <div class="story-text fade-in-left wow">
-                        <p class="section-col f-18 mb-5" style="line-height:25px"><img src="{{ asset('asset/images/dum.jpg')}}" class="float-left" width="80" /> <span class="float-left ml-4">Fresh Hot Pizza in 30 min or Less -<br />Domino's Pizza</span></p>
-                        <div class="clearfix"></div>
-                        <p class="f-16 mt-5"><img src="{{ asset('asset/images/imp.jpg')}}" /> Impressions</p>
-                        <p class="f-16"><img src="{{ asset('asset/images/reach.jpg')}}" /> Reach</p>
-                        <p class="f-16"><img src="{{ asset('asset/images/number.jpg')}}" /> Number of Influencers</p>
-                        
-                        
-                    </div>
-                </div>                
-            </div>
-          
+         <div class="row m-0 position-rel">
+            <img src="{{ asset('asset/images/story-bg.jpg')}}" />
+            <div class="container">
+                <div class="story-text">
+                    <p class="section-col f-18 mb-5 font-semi-bold" style="line-height:25px">Lorem ipsum dolor sit amet,<br>Consectetur adipiscing elit.<br>Etiam facilisis ornare<br>magna, eu rhoncus justo venenatis id</p>
+                    <p class="f-16 font-semi-bold"><img src="{{ asset('asset/images/imp.jpg')}}" /> Impressions</p>
+                    <p class="f-16 font-semi-bold"><img src="{{ asset('asset/images/reach.jpg')}}" /> Reach</p>
+                    <p class="f-16 font-semi-bold"><img src="{{ asset('asset/images/number.jpg')}}" /> Number of Influencers</p>
+                    
+                    
+                </div>
+            </div>                
+        </div>
           
     </section>
     
